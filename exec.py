a4beca2b987201bb1c64ff722fa90f6eab590768
Antoine <antoine.bonnotte@gmail.com>
2022-06-06 12:32:46 +0200

    su

--- exec.py
+++ exec.py
@@ -11,11 +11,28 @@ import arbre_couvrant_pds_min as abr
 # graphe=[[0,2,3,4],[1,2,4],[0,2,3,4],[1,2,3,4],[0,2,4]]
 graphe=[[4,6,8,9],[1,2,7,9],[0,2,9],[1,4,5,6,8,9],[1,8,9],[3,4,6,9],[2,3,5,6,8,9],[3,4,5,6,8,9],[0,1,3,6,8,9],[0,1,2,5,7,9]]
 # graphe=[[0,1,2,6,7,9],[1,8,9],[1,3,5,8,9],[0,2,3,4,6,9],[1,3,4,7,9],[1,4,9],[4,6,7,9],[1,2,5,9],[0,3,5,6,9],[2,5,9]]
-#graphe = [[1,3], [4], [4,5], [], [3], []]
+# graphe = [[1,3], [4], [4,5], [], [3], []]
 # graphe = [[1,3], [2,5], [], [4],[], [6],[]]
-graphe=[[1,3],[2,],[0,3],[5],[3],[4]]
-print("le graphe de départ est ")
-print(graphe)
+# graphe=[[1,3],[2,],[0,3],[5],[3],[4]]
+
+# Debut séance 1
+graphe1=[[0,2,3,4],[1,2,4],[0,2,3,4],[1,2,3,4],[0,2,4]]
+graphe2=[[4,6,8,9],[1,2,7,9],[0,2,9],[1,4,5,6,8,9],[1,8,9],[3,4,6,9],[2,3,5,6,8,9],[3,4,5,6,8,9],[0,1,3,6,8,9],[0,1,2,5,7,9]]
+graphe3=[[0,1,2,6,7,9],[1,8,9],[1,3,5,8,9],[0,2,3,4,6,9],[1,3,4,7,9],[1,4,9],[4,6,7,9],[1,2,5,9],[0,3,5,6,9],[2,5,9]]
+
+matrice1=imp.calc_matrice_adj(graphe1)
+matrice2=imp.calc_matrice_adj(graphe2)
+matrice3=imp.calc_matrice_adj(graphe3)
+
+print(imp.roy_warshall_for_matrice(matrice1))
+# print(imp.roy_warshall_for_matrice(matrice2))
+# print(imp.roy_warshall_for_matrice(matrice3))
+
+
+
+
+# print("le graphe de départ est ")
+# print(graphe)
 # def print_seance1():
     # Séance 1
 # matrix = imp.calc_matrice_adj(graphe)
@@ -28,7 +45,7 @@ print(graphe)
 # graphe = [[1,2], [2], [3], []]
 
 matrice2=imp.matrice_predecesseur(graphe)
-print(matrice2)
+# print(matrice2)
     # imp.var_dump_table(matrix)
     # print("Matrice de Roy Warshall (matrice)")
     # matrix = imp.roy_warshall_for_matrice(matrix)
